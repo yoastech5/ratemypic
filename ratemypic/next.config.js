63179eb81af6ff2c4436fc6e46b9e0801f79b8e5
yoastech5 <yoastech5@gmail.com>
2025-12-06 04:12:11 +0300

    update next.js

--- ratemypic/next.config.js
+++ ratemypic/next.config.js
@@ -6,7 +6,16 @@ const nextConfig = {
         protocol: 'https',
         hostname: '**.supabase.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
     ],
+    unoptimized: false,
   },
 }
 
